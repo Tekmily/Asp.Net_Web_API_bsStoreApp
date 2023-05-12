2952661006a95638c3965794947fbbaef098b6d7
Yusuf TEKMİL <yusuftekmil.yt@outlook.com.tr>
2023-05-12 19:17:19 +0300

    v4

---
+++ WebApi/Extentions/ServicesExtentions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+using Repositories.EFCore;
+
+namespace WebApi.Extentions
+{
+    public static class ServicesExtentions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration)=>
+        
+
+            services.AddDbContext<RepositoryContext>(
+                options =>
+            options.UseSqlServer(configuration.GetConnectionString("sqlConnection"))
+            );
+        public static void ConfigureRepositoryManager(this IServiceCollection services)=>
+            services.AddScoped<IRepositoryManager, RepositoryManager>();
+        
+    }
+}
